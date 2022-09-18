d2dc69922fa4a02e51f10c01ae6626e449485f4d
Enes ÇEB <enesceb@hotmail.com>
2022-09-18 14:18:58 +0300

    Template engine, static files, partials render

--- app.js
+++ app.js
@@ -3,9 +3,34 @@ import express  from 'express';
 const app = express()
 const port = 3000;
 
+//ejs template engine
+app.set("view engine", "ejs")
+
+//static file middleware 
+app.use(express.static('public'))
+
 app.get("/", (req, res) =>{
-    res.send('index sayfası')
+    res.render("index");
 })
+app.get("/about", (req, res) =>{
+    res.render("about");
+})
+app.get("/blog", (req, res) =>{
+    res.render("blog");
+})
+app.get("/contact", (req, res) =>{
+    res.render("contact");
+})
+app.get("/projects", (req, res) =>{
+    res.render("projects");
+})
+app.get("/services", (req, res) =>{
+    res.render("services");
+})
+app.get("/gallery", (req, res) =>{
+    res.render("gallery");
+})
+
 
 app.listen(port, ()=> {
     console.log(`app running" ${port}`);
